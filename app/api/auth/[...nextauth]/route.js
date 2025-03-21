2a9a8d14a10a29bb5d6a802b49adc8bf718b2b41
Shivansh235 <shivansh.2023ug4027@iiitranchi.ac.in>
2025-03-21 15:42:02 +0530

    Some changes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -8,10 +8,7 @@ const handler =  NextAuth({
         clientId: process.env.GITHUB_ID,
         clientSecret: process.env.GITHUB_SECRET
       }),
-      GoogleProvider({
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET
-      })
+   
     ]
 })
 
